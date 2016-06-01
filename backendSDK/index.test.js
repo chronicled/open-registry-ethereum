adabd15b774a4d96d0672c3ca4ff6585389ae58a
Johann Barbie <johannbarbie@me.com>
2016-06-01 00:14:46 -0700

    added first test

--- backendSDK/index.test.js
+++ backendSDK/index.test.js
@@ -1,18 +1,76 @@
 var expect = require('chai').expect;
-var Recovery = require('./lib/registrant');
+var Registrant = require('./lib/registrant');
 var ProtoBuf = require("protobufjs");
+var sinon = require('sinon');
+require('chai').use(require('sinon-chai'));
 
+
+var slice1 = '0x1234000000000000000000000000000000000000000000000000000000004321';
+var slice2 = '0x5678000000000000000000000000000000000000000000000000000000004321';
+var total = '0x12340000000000000000000000000000000000000000000000000000000043215678000000000000000000000000000000000000000000000000000000004321';
+
+var proto = "message Identities {    \
+  repeated Identity identities = 1; \
+  optional Data data = 2;           \
+}                                   \
+                                    \
+message Identity {                  \
+  required string uri = 1;          \
+}                                   \
+                                    \
+message Data {                      \
+  optional string MymeType = 1;     \
+  optional string brandName = 2;    \
+}";
+var builder = ProtoBuf.loadJson(ProtoBuf.DotProto.Parser.parse(proto));
+var Identities = builder.build("Identities");
 
 describe('protobuf test', function() {
 
   it('should allow to serialize and deserialize identities.', function(done) {
-    var builder = ProtoBuf.loadProtoFile("./identities.proto");
-    var Identities = builder.build("Identities");
+
     var ids = new Identities({ 
       identities: [ { uri: 'uri' } ],
       data: null
     });
     expect(ids).to.eql(Identities.decodeHex('0a050a03757269'));
     done();
+  });
+
+  it('should allow to split protobuf into 32byte parts.', function(done) {
+    var registrant = new Registrant();
+    var slices = registrant.slice(total);
+    expect(slices[0]).to.eql(slice1);
+    expect(slices[1]).to.eql(slice2);
+    done();
+  });
+
+  it('should allow to concatenate parts back together', function(done) {
+    var registrant = new Registrant();
+    var merged = registrant.merge([slice1, slice2]);
+    expect(merged).to.eql(total);
+    done();
+  });
+});
+
+
+describe('registrant sdk', function() {
+
+  it('should parse protobuf according to schema.', function(done) {
+
+    var contract = { getAsset: { call: function() {} } };
+    sinon.stub(contract.getAsset, 'call').yields(null, [
+      proto,
+      ['0x0a050a03757269'],
+      true
+    ]);
+
+    var registrant = new Registrant(contract);
+
+
+    registrant.getAsset('0x1234').then(function(rv) {
+      expect(JSON.stringify(rv)).to.eql(JSON.stringify(Identities.decodeHex('0a050a03757269')));
+      done();
+    }).catch(done);
   });
 });
--- backendSDK/lib/registrant.js
+++ backendSDK/lib/registrant.js
@@ -1,92 +1,37 @@
-const Web3 = require('web3');
-const ProviderEngine = require('web3-provider-engine');
-const CacheSubprovider = require('web3-provider-engine/subproviders/cache.js');
-const FixtureSubprovider = require('web3-provider-engine/subproviders/fixture.js');
-const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
-const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js');
-const VmSubprovider = require('web3-provider-engine/subproviders/vm.js');
-const HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
-const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker.js');
-const wallet = require('eth-lightwallet');
 var ProtoBuf = require("protobufjs");
-var ByteBuffer = ProtoBuf.ByteBuffer;
 
-const pwDerivedKey = new Uint8Array([215,152,86,175,5,168,43,177,135,97,218,89,136,5,110,93,193,114,94,197,247,212,127,83,200,150,255,124,17,245,91,10]);
-const registryAbi = [{"constant":true,"inputs":[{"name":"_reference","type":"bytes32"}],"name":"getRecord","outputs":[{"name":"","type":"uint256"},{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[],"name":"registrar","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"records","outputs":[{"name":"owner","type":"address"},{"name":"version","type":"uint256"},{"name":"data","type":"string"},{"name":"isValid","type":"bool"}],"type":"function"},{"constant":false,"inputs":[{"name":"_data","type":"string"},{"name":"_version","type":"uint256"},{"name":"_reference","type":"bytes32"},{"name":"_owner","type":"address"}],"name":"createFor","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_data","type":"string"},{"name":"_version","type":"uint256"},{"name":"_reference","type":"bytes32"}],"name":"create","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_reference","type":"bytes32"},{"name":"_isValid","type":"bool"}],"name":"setValid","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"recordIndex","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[],"type":"constructor"}];
-
-function Registrant (registryAddress, secretSeed, rpcUrl) {
-
-  var engine = new ProviderEngine();
-  var web3 = new Web3(engine);
-  
-  engine.addProvider(new FixtureSubprovider({
-    web3_clientVersion: 'ProviderEngine/v0.0.0/javascript',
-    net_listening: true,
-    eth_hashrate: '0x00',
-    eth_mining: false,
-    eth_syncing: true,
-  }));
-
-  // cache layer
-  engine.addProvider(new CacheSubprovider());
-
-  // filters 
-  engine.addProvider(new FilterSubprovider())
-
-  // pending nonce
-  engine.addProvider(new NonceSubprovider());
-
-  // vm
-  //engine.addProvider(new VmSubprovider());
-
-  var ks = new wallet.keystore(secretSeed, pwDerivedKey);
-  ks.generateNewAddress(pwDerivedKey, 1);
-  var addr = '0x' + ks.getAddresses()[0];
-
-  engine.addProvider(new HookedWalletSubprovider({
-    getAccounts: function(cb) {
-      cb(null, [addr]);
-    },
-    approveTransaction: function(txParams, cb) {
-      cb(null, true);
-    },
-    signTransaction: function(txData, cb) {
-      txData.gasPrice = parseInt(txData.gasPrice, 16);
-      txData.nonce = parseInt(txData.nonce, 16);
-      txData.gasLimit = txData.gas;
-      var tx = wallet.txutils.createContractTx(addr, txData);
-      var signed = wallet.signing.signTx(ks, pwDerivedKey, tx.tx, addr);
-      cb(null, signed);
-    }
-  }));
-
-  engine.addProvider(new RpcSubprovider({
-    rpcUrl: rpcUrl,
-  }));
-  engine.start();
-
-  this.registry = web3.eth.contract(registryAbi).at(registryAddress);
-
+function Registrant (contract, addr, web3) {
+  this.registry = contract;
   this.address = addr;
   this.web3 = web3;
+}
+
+
+//should use buffer to handle this
+Registrant.prototype.slice = function (bytes) {
+  bytes = bytes.replace('0x','');
+  var slices = [];
+  while (bytes.length > 64) {
+    slices.push('0x' + bytes.substring(0, 64));
+    bytes = bytes.substring(64, bytes.length);
+  }
+  slices.push('0x' + bytes);
+  return slices;
+}
+
+//should use buffer to handle this
+Registrant.prototype.merge = function (bytes32Array) {
+  var merged = '0x';
+  for (var i = 0; i < bytes32Array.length; i++) {
+    merged += bytes32Array[i].replace('0x','');
+  }
+  return merged;
 }
 
 Registrant.prototype.create = function (data, version, reference) {
   var self = this;
   return new Promise(function (fulfill, reject) {
     self.registry.create(data, version, reference, {from: self.address}, function(err, data) {
-      if (err) {
-        reject(err);
-      }
-      fulfill(data);
-    });
-  });
-}
-
-Registrant.prototype.createFor = function (data, version, reference, owner) {
-  var self = this;
-  return new Promise(function (fulfill, reject) {
-    self.registry.create(data, version, reference, owner, {from: self.address}, function(err, data) {
       if (err) {
         reject(err);
       }
@@ -107,17 +52,24 @@ Registrant.prototype.setValid = function (reference, isValid) {
   });
 }
 
-Registrant.prototype.getRecord = function (reference) {
+Registrant.prototype.getAsset = function (reference) {
   var self = this;
   return new Promise(function (fulfill, reject) {
-    self.registry.getRecord.call(reference, {from: self.address}, function(err, data) {
+    self.registry.getAsset.call(reference, {from: self.address}, function(err, data) {
       if (err) {
         reject(err);
       }
-      fulfill({
-        v: data[0].toNumber(),
-        data: data[1]
-      });
+      var schema = data[0];
+      var identities = data[1];
+      var isValid = data[2];
+      if (!isValid) {
+        reject('Error: record marked as invalid.');
+      }
+      var builder = ProtoBuf.loadJson(ProtoBuf.DotProto.Parser.parse(schema));
+      var Identities = builder.build("Identities");
+      var merged = self.merge(identities);
+      var decoded = Identities.decodeHex(merged.replace('0x',''));
+      fulfill(decoded);
     });
   });
 }
