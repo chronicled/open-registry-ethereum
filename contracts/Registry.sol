e4fceba23ea2cb7e1792c759f46db3236a994027
Johann Barbie <johannbarbie@me.com>
2016-06-20 11:32:23 +0200

    made ids separate from data and unique

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -2,16 +2,18 @@ import "Registrar.sol";
 
 contract Registry {
     
+    event Create(address indexed owner, bytes32 indexed identity, uint position);
+    
     address public registrarAddress;
     
     struct Thing {
         address ownerAddress;
         uint schemaReference;
-        bytes32[] identities;
+        bytes32[] data;
         bool isValid;
     }
 
-    mapping(bytes32 => uint) public thingReferences;
+    mapping(bytes32 => uint) public identities;
     
     Thing[] public things;
     string[] public schemas;
@@ -38,34 +40,46 @@ contract Registry {
     //################# INTERNAL FUNCTIONS
     
     
-    function _create(address _owner, uint _schemaIndex, bytes32[] _identities, bytes32 _thingReference) internal returns (uint) {
-        if (thingReferences[_thingReference] > 0) {
-            throw;
+    function _create(address _owner, uint _schemaIndex, bytes32[] _data, bytes32[] _identities) internal returns (uint) {
+        for (uint i = 0; i < _identities.length; i++) {
+            if (identities[_identities[i]] > 0) {
+                throw;
+            }
         }
         uint pos = things.length++;
         //todo: validate schema version
-        things[pos] = Thing(_owner, _schemaIndex, _identities, true);
-        thingReferences[_thingReference] = pos;
+        things[pos] = Thing(_owner, _schemaIndex, _data, true);
+        for (uint k = 0; k < _identities.length; k++) {
+            identities[_identities[k]] = pos;
+            Create(_owner, _identities[k], pos);
+        }
         return pos;
     }
     
+    //same call like create, but data size can only be 1 time 32 bytes
+    function _create1(address _owner, uint _schemaIndex, bytes32 _data, bytes32[] _identities) internal returns (uint) {
+        bytes32[] memory datas = new bytes32[](1);
+        datas[0] = _data;
+        _create(_owner, _schemaIndex, datas, _identities);
+    }
+    
     function _setValid(bytes32 _thingReference, bool _isValid) internal {
-        uint pos = thingReferences[_thingReference];
+        uint pos = identities[_thingReference];
         if (pos == 0) {
             throw;
         }
         things[pos].isValid = _isValid;
     }
     
-    function _linkReference(address _owner, uint _pos, bytes32 _thingReference) internal {
-        if (_pos > things.length || thingReferences[_thingReference] > 0) {
+    function _linkIdentity(address _owner, uint _pos, bytes32 _identity) internal {
+        if (_pos > things.length || identities[_identity] > 0) {
             throw;
         }
         Thing thing = things[_pos];
         if (things[_pos].ownerAddress != _owner) {
             throw;
         }
-        thingReferences[_thingReference] = _pos;
+        identities[_identity] = _pos;
     }
     
     //################# PUBLIC FUNCTIONS
@@ -77,40 +91,33 @@ contract Registry {
         registrarAddress = _registrarAddress;
     }
     
-    //batch create: open question: how to deal with array of dynamic types?
-    function createMany(uint[] _schemaIndex, uint8[] _identityLength, bytes32[] _identities, bytes32[] _references) isRegistrant(msg.sender) returns (uint, uint) {
+    //this function allows entries with _data of 1 times 32bytes only;
+    function createMany(uint _schemaIndex, bytes32[] _data, uint8[] _idLength, bytes32[] _identities) isRegistrant(msg.sender) returns (uint, uint) {
         uint startPosition = things.length;
-        uint identityPosition = 0;
-        for (uint i = 0; i < _schemaIndex.length; i++) {
-            uint8 length = _identityLength[i];
+        uint thingPosition = 0;
+        for (uint i = 0; i < _data.length; i++) {
+            uint8 length = _idLength[i];
             bytes32[] memory ids = new bytes32[](length);
             for (uint j = 0; j < length; j++) {
-                ids[j] = _identities[identityPosition + j];
+                ids[j] = _identities[thingPosition + j];
             }
-            identityPosition += length;
-            _create(msg.sender, _schemaIndex[i], ids, _references[i]);
-            // TODO: deal with references
-            // for (uint k = 1; k < length; k++) {
-            //     _linkAssetReference(msg.sender, pos, _references[identityPosition + k]);
-            // }
+            thingPosition += length;
+            _create1(msg.sender, _schemaIndex, _data[i], ids);
         }
-        return (startPosition, startPosition + _schemaIndex.length);
+        return (startPosition, startPosition + _data.length);
     }
     
-    function create(uint _schemaIndex, bytes32[] _identities, bytes32[] _references) isRegistrant(msg.sender) returns (uint) {
-        uint pos = _create(msg.sender, _schemaIndex, _identities, _references[0]);
-        for (uint i = 1; i < _references.length; i++) {
-            _linkReference(msg.sender, pos, _references[i]);
-        }
+    function create(uint _schemaIndex, bytes32[] _data, bytes32[] _identities) isRegistrant(msg.sender) returns (uint) {
+        uint pos = _create(msg.sender, _schemaIndex, _data, _identities);
+        return pos;
     }
     
-    function linkReference(uint _pos, bytes32 _reference) isRegistrant(msg.sender) {
-        _linkReference(msg.sender, _pos, _reference);
+    function linkIdentity(uint _pos, bytes32 _identity) isRegistrant(msg.sender) {
+        _linkIdentity(msg.sender, _pos, _identity);
     }
     
-    function setValid(bytes32 _reference, bool _isValid) isRegistrant(msg.sender) returns (bool) {
-        _setValid(_reference, _isValid);
-        //Open question: what if we want to update the record for an item? we can not simply set it inValid, and create a new one, because the references are already taken
+    function setValid(bytes32 _identity, bool _isValid) isRegistrant(msg.sender) returns (bool) {
+        _setValid(_identity, _isValid);
     }
     
     function addSchema(string _schema) isCertificationAuthority(msg.sender) returns (uint) {
@@ -122,13 +129,13 @@ contract Registry {
     //################# CONSTANT FUNCTIONS
     
     
-    function getThing(bytes32 _thingReference) constant returns (string, bytes32[], bool) {
-        uint pos = thingReferences[_thingReference];
+    function getThing(bytes32 _identity) constant returns (string, bytes32[], bool) {
+        uint pos = identities[_identity];
         if (pos == 0) {
             throw;
         }
         Thing thing = things[pos];
-        return (schemas[thing.schemaReference], thing.identities, thing.isValid);
+        return (schemas[thing.schemaReference], thing.data, thing.isValid);
     }
     
 }
