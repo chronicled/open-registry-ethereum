6089e5f997c8abd278cc1208aba3f171339a39da
Johann Barbie <johannbarbie@me.com>
2016-07-13 14:47:20 +0200

    reverted commits that broke test, encoding and permissions

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -1,26 +1,22 @@
 contract Registrar {
-
+    
     address public certificationAuthority;
     address public registry;
-
-    event Creation(address indexed registrant, address authority, string name, string contact, string legalName, string legalAddress, bool active);
-    event Alternation(address indexed registrant, address authority, string name, string contact, string legalName, string legalAddress, bool active);
+    
+    event Creation(address indexed registrant, address authority, string reference);
+    event Alternation(address indexed registrant, address authority, bool isActive, string reference);
      //1: permission denied
     event Error(uint code);
-
+    
     struct Registrant {
         address addr;
-        string name;
-        string description;
-        string contact;
-        string legalName;
-        string legalAddress;
-        bool active;
+        string reference;
+        bool isActive;
     }
 
     mapping(address => uint) public registrantIndex;
     Registrant[] public registrants;
-
+    
     function Registrar() {
         certificationAuthority = msg.sender;
         registrants.length++;
@@ -30,53 +26,41 @@ contract Registrar {
         address[] memory rv = new address[](registrants.length);
         uint counter = 0;
         for (uint i = 0; i < registrants.length; i++) {
-            if (registrants[i].active) {
+            if (registrants[i].isActive) {
                 rv[counter] = registrants[i].addr;
                 counter++;
             }
         }
         return rv;
     }
-
-    function getRegistrant(address _registrant) constant returns(string, string, string, string, string, bool){
-        uint pos = registrantIndex[_registrant];
-        if (pos > 0)
-            return (registrants[pos].name, registrants[pos].description, registrants[pos].contact, registrants[pos].legalName, registrants[pos].legalAddress, registrants[pos].active);
-        return ("", "", "", "", "" , false);
-    }
-
+    
     function isActiveRegistrant(address _registrant) constant returns (bool) {
         uint pos = registrantIndex[_registrant];
-        return (pos > 0 && registrants[pos].active);
+        return (pos > 0 && registrants[pos].isActive);
     }
-
-    function add(address _registrant, string _name, string _description, string _contact, string _legalName, string _legalAddress) returns (bool) {
+    
+    function add(address _registrant, string _reference) returns (bool) {
         if (msg.sender != certificationAuthority || registrantIndex[_registrant] > 0) {
             Error(1); //permission denied
             return false;
         }
-        uint pos = registrants.length ++;
-        registrants[pos] = Registrant(_registrant, _name, _description, _contact, _legalName, _legalAddress, true);
+        uint pos = registrants.length++;
+        registrants[pos] = Registrant(_registrant, _reference, true);
         registrantIndex[_registrant] = pos;
-        Creation(_registrant, msg.sender, _name, _contact, _legalName, _legalAddress, true);
+        Creation(_registrant, msg.sender, _reference);
     }
-
-    function edit(address _registrant, string _name, string _description, string _contact, string _legalName, string _legalAddress, bool _active) returns (bool) {
+    
+    function setActive(address _registrant, bool _isActive, string _reference) returns (bool) {
         if (msg.sender != certificationAuthority || registrantIndex[_registrant] == 0) {
             Error(1); //permission denied
             return false;
         }
         Registrant registrant = registrants[registrantIndex[_registrant]];
-        registrant.active = _active;
-        registrant.name = _name;
-        registrant.description = _description;
-        registrant.contact = _contact;
-        registrant.legalName = _legalName;
-        registrant.legalAddress = _legalAddress;
-        registrant.active = _active;
-        Alternation(_registrant, msg.sender, _name, _contact, _legalName, _legalAddress, _active);
+        registrant.isActive = _isActive;
+        registrant.reference = _reference;
+        Alternation(_registrant, msg.sender, _isActive, _reference);
     }
-
+    
     function setNextAuthority(address _ca) returns (bool) {
         if (msg.sender != certificationAuthority) {
             Error(1); //permission denied
@@ -84,5 +68,5 @@ contract Registrar {
         }
         certificationAuthority = _ca;
     }
-
+    
 }
--- truffle.js
+++ truffle.js
@@ -16,8 +16,7 @@ module.exports = {
     "MultiAccessTester"
   ],
   rpc: {
-    host: "104.197.248.244",
-    port: 8555,
-    from: "300221400d539cb5d15940c56239e6353287eba2"
+    host: "localhost",
+    port: 8545
   }
 };
