361d26f103f6f195970dad6c8ca8527a9c05e2eb
Johann Barbie <johannbarbie@me.com>
2016-07-14 10:52:27 +0200

    added maksym's proposal. WIP as not compiling

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -13,12 +13,20 @@ contract Registry {
     // 6: bad request, at least one data needed
     event Error(uint code, bytes32 reference);
     
+    struct Identity {
+        bytes16 schema;
+        bytes16 idA;
+        bytes32 idB;
+    }
+    
     struct Thing {
         address ownerAddress;
+        Identity[] identities;
         uint schemaReference;
         bytes32[] data;
         bool isValid;
     }
+    
 
     mapping(bytes32 => uint) public identities;
     
@@ -51,17 +59,17 @@ contract Registry {
     
     //################# INTERNAL FUNCTIONS
     
-    function _create(address _caller, uint _schemaIndex, bytes32[] _data, bytes32[] _identities) internal returns (bool) {
+    function _create(address _caller, uint _schemaIndex, bytes32[] _data, bytes16[] _schema, bytes16[] _idA, bytes32[] _idB) internal returns (bool) {
         uint pos = things.length++;
-        return _update(_caller, pos, _schemaIndex, _data, _identities);
+        return _update(_caller, pos, _schemaIndex, _data, _schema, _idA, _idB);
     }
     
-    function _update(address _caller, uint _pos, uint _schemaIndex, bytes32[] _data, bytes32[] _identities) internal returns (bool) {
+    function _update(address _caller, uint _pos, uint _schemaIndex, bytes32[] _data, bytes16[] _schema, bytes16[] _idA, bytes32[] _idB) internal returns (bool) {
         if (_schemaIndex > schemas.length) {
-            Error(4, _identities[0]);
+            Error(4, 0);
             return false;
         }
-        if (_identities.length == 0) {
+        if (_schema.length == 0 || _idA.length == 0 || _idB.length == 0) {
             Error(5, 0);
             return false;
         }
@@ -70,21 +78,25 @@ contract Registry {
             return false;
         }
         if (things[_pos].ownerAddress != 0x0 && things[_pos].ownerAddress != _caller) {
-            Error(3, _identities[0]);
+            Error(3, 0);
             return false;
         }
-        for (uint i = 0; i < _identities.length; i++) {
-            uint previous = identities[_identities[i]];
+        Identity[] memory ids = new Identity[](_idA.length);
+        for (uint i = 0; i < _idA.length; i++) {
+            ids[i] = Identity(_schema[i], _idA[i], _idB[i]);
+            bytes32 hash = sha3(_schema[i],_idA[i],_idB[i]);
+            uint previous = identities[hash];
             if (previous > 0 && previous != _pos) {
-                Error(1, _identities[i]);
+                Error(1, hash);
                 return false;
             }
         }
-        things[_pos] = Thing(_caller, _schemaIndex, _data, true);
-        for (uint k = 0; k < _identities.length; k++) {
-            if (identities[_identities[k]] == 0) {
-                identities[_identities[k]] = _pos;
-                Creation(_identities[k], _caller, _pos);
+        things[_pos] = Thing(_caller, ids, _schemaIndex, _data, true);
+        for (uint k = 0; k < _idA.length; k++) {
+            hash = sha3(_schema[i],_idA[i],_idB[i]);
+            if (identities[hash] == 0) {
+                identities[hash] = _pos;
+                Creation(hash, _caller, _pos);
             }
         }
         return true;
@@ -105,18 +117,20 @@ contract Registry {
         return true;
     }
     
-    function _linkIdentity(address _caller, uint _pos, bytes32 _identity) internal returns (bool) {
-        if (_pos > things.length || identities[_identity] > 0) {
-            Error(2, _identity);
+    function _linkIdentity(address _caller, uint _pos, bytes16 _schema, bytes16 _idA, bytes32 _idB) internal returns (bool) {
+        bytes32 hash = sha3(_schema, _idA, _idB);
+        if (_pos > things.length || identities[hash] > 0) {
+            Error(2, hash);
             return false;
         }
         Thing thing = things[_pos];
         if (things[_pos].ownerAddress != _caller) {
-            Error(3, _identity);
+            Error(3, hash);
             return false;
         }
-        identities[_identity] = _pos;
-        Alternation(_identity, _caller, things[_pos].isValid, _pos);
+        things[_pos].identities[things[_pos].identities.length++] = Identity(_schema, _idA, _idB);
+        identities[hash] = _pos;
+        Alternation(hash, _caller, things[_pos].isValid, _pos);
         return true;
     }
     
@@ -131,29 +145,33 @@ contract Registry {
         return true;
     }
     
-    function create(uint _schemaIndex, bytes32[] _data, bytes32[] _identities) isRegistrant(msg.sender) noEther returns (bool) {
-        return _create(msg.sender, _schemaIndex, _data, _identities);
+    function create(uint _schemaIndex, bytes32[] _data, bytes16[] _schema, bytes16[] _idA, bytes32[] _idB) isRegistrant(msg.sender) noEther returns (bool) {
+        return _create(msg.sender, _schemaIndex, _data, _schema, _idA, _idB);
     }
     
-    function update(uint _pos, uint _schemaIndex, bytes32[] _data, bytes32[] _identities) isRegistrant(msg.sender) noEther returns (bool) {
-        return _update(msg.sender, _pos, _schemaIndex, _data, _identities);
+    function update(uint _pos, uint _schemaIndex, bytes32[] _data, bytes16[] _schema, bytes16[] _idA, bytes32[] _idB) isRegistrant(msg.sender) noEther returns (bool) {
+        return _update(msg.sender, _pos, _schemaIndex, _data, _schema, _idA, _idB);
     }
     
-    //this function allows entries with _identities of 1 times 32bytes only; others have to be added through linkIdentity later
+    // this function allows entries with _identities of 1 times 32bytes only; others have to be added through linkIdentity later
     // Review: user should be aware that if there will be not enough identities transaction will run out of gas.
     // Review: user should be aware that providing too many identities will result in some of them not being used.
-    function createMany(uint _schemaIndex, uint8[] _dataLength, bytes32[] _data, bytes32[] _identities) isRegistrant(msg.sender) noEther returns (bool) {
+    function createMany(uint _schemaIndex, uint8[] _dataLength, bytes32[] _data, bytes16[] _schema, bytes16[] _idA, bytes32[] _idB) isRegistrant(msg.sender) noEther returns (bool) {
         uint thingPosition = 0;
-        for (uint i = 0; i < _identities.length; i++) {
+        for (uint i = 0; i < _schema.length; i++) {
             uint8 length = _dataLength[i];
             bytes32[] memory datas = new bytes32[](length);
             for (uint j = 0; j < length; j++) {
                 datas[j] = _data[thingPosition + j];
             }
             thingPosition += length;
-            bytes32[] memory ids = new bytes32[](1);
-            ids[0] = _identities[i];
-            _create(msg.sender, _schemaIndex, datas, ids);
+            bytes16[] memory schemas = new bytes16[](1);
+            schemas[0] = _schema[i];
+            bytes16[] memory idAs = new bytes16[](1);
+            idAs[0] = _idA[i];
+            bytes32[] memory idBs = new bytes32[](1);
+            idBs[0] = _idB[i];
+            _create(msg.sender, _schemaIndex, datas, schemas, idAs, idBs);
         }
         // Review: maybe check if there is identities left and throw if so? => done
         if (thingPosition != _data.length) {
@@ -162,8 +180,8 @@ contract Registry {
         return true;
     }
     
-    function linkIdentity(uint _pos, bytes32 _identity) isRegistrant(msg.sender) noEther returns (bool success) {
-        return _linkIdentity(msg.sender, _pos, _identity);
+    function linkIdentity(uint _pos, bytes16 _schema, bytes16 _idA, bytes32 _idB) isRegistrant(msg.sender) noEther returns (bool success) {
+        return _linkIdentity(msg.sender, _pos, _schema, _idA, _idB);
     }
     
     function setValid(bytes32 _identity, bool _isValid) isRegistrant(msg.sender) noEther returns (bool) {
